7917f076f817f4ef087d29c9212bae6e5c1d0169
Arjun Nagineni <y2kpr@Arjuns-iMac.local>
2016-10-22 17:14:26 -0500

    Added PDF generator function

---
+++ deskThree/deskThree/PdfGenerator.swift
@@ -0,0 +1,31 @@
+//
+//  PdfGenerator.swift
+//  deskThree
+//
+//  Created by Arjun Nagineni on 10/22/16.
+//  Copyright © 2016 desk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PdfGenerator: NSObject {
+    
+    func createPdfFromView(aView: UIView, saveToDocumentsWithFileName fileName: String)
+    {
+        let pdfData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData, aView.bounds, nil)
+        UIGraphicsBeginPDFPage()
+        
+        guard let pdfContext = UIGraphicsGetCurrentContext() else { return }
+        
+        aView.layer.render(in: pdfContext)
+        UIGraphicsEndPDFContext()
+        
+        if let documentDirectories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let documentsFileName = documentDirectories + "/" + fileName
+            debugPrint(documentsFileName)
+            pdfData.write(toFile: documentsFileName, atomically: true)
+        }
+    }
+}
