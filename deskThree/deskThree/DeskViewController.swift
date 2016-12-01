ab39d78fc5457d864273b4d2af8a4f82c862c56e
Cage <cagejohnson@utexas.edu>
2016-12-01 16:08:36 -0600

    some more changes

--- deskThree/deskThree/DeskViewController.swift
+++ deskThree/deskThree/DeskViewController.swift
@@ -9,29 +9,27 @@ import UIKit
 
 class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecognizerDelegate, UIDocumentInteractionControllerDelegate, UINavigationControllerDelegate, GKImagePickerDelegate, JotViewDelegate, JotViewStateProxyDelegate, InputObjectDelegate, ExpressionDelegate {
     
-    var jotViewStateInkPath: String!
-    var jotViewStatePlistPath: String!
-    
-   
-   // let imagePicker = UIImagePickerController()
     let gkimagePicker = GKImagePicker()
-    @IBOutlet var workArea: WorkArea!
-    var singleTouchPanGestureRecognizer: UIPanGestureRecognizer!
     
-    var pen: Pen!
+    @IBOutlet var workArea: WorkArea!
+   
+    //calculator properties
     var allPad: InputObject?
     var isPadActive: Bool = false
-    var jotView: JotView!
-    var paperState: JotViewStateProxy!
-    
     var expressions: [Expression] = []
 
-    
-    
+    //JotUI Properties
+    var pen: Pen!
+    var jotView: JotView!
+    var paperState: JotViewStateProxy!
+    var jotViewStateInkPath: String!
+    var jotViewStatePlistPath: String!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //workArea is loaded from Nib
         workArea.delegate = self
         self.view.sendSubview(toBack: workArea)
         workArea.minimumZoomScale = 0.3
@@ -41,110 +39,30 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         gkimagePicker.cropSize = CGSize(width: 320, height: 320)
         gkimagePicker.resizeableCropArea = true
        
+        //JotUI setup
         pen = Pen()
-        
         jotView = JotView(frame: CGRect(x: 0, y: 0, width: 1275, height: 1650))
-        //jotView = JotView(frame: CGRect(x: 0, y: 0, width: 600 , height: 600))
-        
-        //jotView = JotView(frame: CGRect(x: 0, y: 0, width: 1236, height: 1600))
-        //var jotView = JotView(frame: self.view.frame)
-
         jotView.delegate = self
-        
         workArea.currentPage.addSubview(jotView)
-        //self.view.addSubview(jotView)
-        
-        jotView.isUserInteractionEnabled = true 
-        
+        jotView.isUserInteractionEnabled = true
         paperState = JotViewStateProxy(delegate: self)
         paperState?.delegate = self
-        
         paperState?.loadJotStateAsynchronously(false, with: jotView.bounds.size, andScale: UIScreen.main.scale, andContext: jotView.context, andBufferManager: JotBufferManager.sharedInstance())
-        
         jotView.loadState(paperState)
-        
-
-    /*
-        
-        JotViewStateProxy* paperState = [[JotViewStateProxy alloc] initWithDelegate:self];
-        paperState.delegate = self;
-        
-        
-        [paperState loadJotStateAsynchronously:NO withSize:jotView.bounds.size andScale:[[UIScreen mainScreen] scale] andContext:jotView.context andBufferManager:[JotBufferManager sharedInstance]];
-        [jotView loadState:paperState];
-        
-        [self changePenType:nil];
-        
-        [self tappedColorButton:blackButton];
-        
-        marker.color = [redButton backgroundColor];
-        pen.color = [blackButton backgroundColor];
-        
-        
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-       // var metalDevice = MTLCreateSystemDefaultDevice()
-      //  let metalCommandQueue = metalDevice?.makeCommandQueue()
-        
-     //   var activeDrawing: ActiveDrawing = ActiveDrawing(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 44), device: metalDevice)
-        
-     //   activeDrawing.commandQueue = metalCommandQueue
-        //activeDrawing.loadUIImage(image: UIImage(named: "wave")!)
-        
-     //   var storedDrawing: StoredDrawing = StoredDrawing(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 44), device: metalDevice, activeResource: activeDrawing)
-    //    storedDrawing.commandQueue = metalCommandQueue
-    //    storedDrawing.loadUIImage(image: UIImage(named: "apple")!)
-     //   self.view.addSubview(storedDrawing)
-   //     self.view.addSubview(activeDrawing)
-
-        
-        
-        
-        /*
-        workArea = WorkArea()
-        self.view.addSubview(workArea)
-        workArea.boundInsideBy(superView: self.view, x1: 10, x2: 10, y1: 10, y2: 44)
-        
-        workArea.minimumZoomScale = 0.1
-        workArea.maximumZoomScale = 2.0
-        */
-        // setting up the GR that will handle drawing w finger & stylus
-        singleTouchPanGestureRecognizer = UIPanGestureRecognizer(target: workArea.currentPage, action: #selector(Paper.handlePan))
-        singleTouchPanGestureRecognizer.minimumNumberOfTouches = 1
-        singleTouchPanGestureRecognizer.maximumNumberOfTouches = 1
-        //singleTouchPanGestureRecognizer.isEnabled = true
-        singleTouchPanGestureRecognizer.isEnabled = false
-        singleTouchPanGestureRecognizer.delegate = self
-        //self.view.addGestureRecognizer(singleTouchPanGestureRecognizer)
- 
     }
     
     override func viewDidAppear(_ animated: Bool) {
     }
  
-    
     func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
         return self
-    }
-    
-    //function to handle the drawing (is in paper.swift)
-    func handleSinglePan(sender: UIPanGestureRecognizer) {
-        // workArea.currentPage.handlePan(sender: sender)
-        // self.view.backgroundColor = UIColor.green
-      //  self.workArea.isHidden = true
     }
     
     //MARK: - WorkArea Delegate
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return workArea.currentPage
     }
+    
     @IBAction func setImagesButtonPushed(_ sender: AnyObject) {
         for i in 0 ..< workArea.currentPage.images!.count {
             workArea.currentPage.images![i].toggleEditable()
@@ -157,6 +75,7 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         allPad?.delegate = self
     }
     
+    //need to work on this
     func hello(imageV: UIImage?){
         var useful: UIImageView = UIImageView(image: imageV)
         workArea.currentPage.addSubview(useful)
@@ -169,11 +88,8 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         useful.removeFromSuperview()
     }
     
-    
     //MARK: UIToolbar on click methods
     @IBAction func printButtonPushed(_ sender: UIBarButtonItem) {
-
-
         jotView.exportToImage(onComplete: hello , withScale: (workArea.currentPage.image?.scale)!)
         //workArea.frame = workArea.currentPage.frame
 //        var pdfFileName = PDFGenerator.createPdfFromView(aView: workArea.currentPage, saveToDocumentsWithFileName: "secondPDF")
@@ -187,17 +103,15 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
     }
 
     @IBAction func undoButtonPressed(_ sender: AnyObject) {
-        jotView.undo()
+    jotView.undo()
     }
     
-    
-    
     @IBAction func loadImageButtonPushed(_ sender: UIBarButtonItem) {
-        present(gkimagePicker.imagePickerController, animated: true, completion: nil)
-        }
+    present(gkimagePicker.imagePickerController, animated: true, completion: nil)
+    }
     
     @IBAction func clearButtonTapped(_ sender: AnyObject) {
-        jotView.clear(true);
+    jotView.clear(true);
     }
 // MARK: GKImagePickerController Delegate
 @objc func imagePicker(_ imagePicker: GKImagePicker,  pickedImage: UIImage) {
@@ -219,16 +133,12 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         // Dispose of any resources that can be recreated.
     }
     
-    
     //pragma mark - Helpers
-    
     func activePen() -> Pen {
         return pen
     }
-    
 
     //JotUIDelegate
-    
     func textureForStroke() -> JotBrushTexture! {
         return activePen().textureForStroke()
     }
@@ -237,16 +147,13 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         return activePen().stepWidthForStroke()
     }
     
-    
     func supportsRotation() -> Bool {
         return activePen().supportsRotation()
     }
   
-    
     func willAddElements(_ elements: [Any]!, to stroke: JotStroke!, fromPreviousElement previousElement: AbstractBezierPathElement!) -> [Any]! {
         return activePen().willAddElements(elements, to: stroke, fromPreviousElement: previousElement)
     }
-    
     
     func willBeginStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> Bool {
         activePen().willBeginStroke(withCoalescedTouch: coalescedTouch, from: touch)
@@ -256,7 +163,6 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
     func willMoveStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) {
         activePen().willMoveStroke(withCoalescedTouch: coalescedTouch, from: touch)
     }
-    
     
     func willEndStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!, shortStrokeEnding: Bool) {
         //noop
@@ -270,7 +176,6 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         activePen().willCancel(stroke, withCoalescedTouch: coalescedTouch, from: touch)
     }
     
-    
     func didCancel(_ stroke: JotStroke!, withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) {
         activePen().didCancel(stroke, withCoalescedTouch: coalescedTouch, from: touch)
     }
@@ -281,32 +186,15 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         return activePen().color(forCoalescedTouch: coalescedTouch, from: touch)
     }
     
-    /*
-    - (UIColor*)colorForCoalescedTouch:(UITouch*)coalescedTouch fromTouch:(UITouch*)touch {
-    [[self activePen] setShouldUseVelocity:pressureVsVelocityControl.selectedSegmentIndex];
-    return [[self activePen] colorForCoalescedTouch:coalescedTouch fromTouch:touch];
-    }
-    */
-    
     func width(forCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> CGFloat {
         //activePen().shouldUseVelocity
         return activePen().width(forCoalescedTouch: coalescedTouch, from: touch)
     }
     
-    /*
-    - (CGFloat)widthForCoalescedTouch:(UITouch*)coalescedTouch fromTouch:(UITouch*)touch {
-    [[self activePen] setShouldUseVelocity:pressureVsVelocityControl.selectedSegmentIndex];
-    return [[self activePen] widthForCoalescedTouch:coalescedTouch fromTouch:touch];
-    }
-    */
-    
     func smoothness(forCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> CGFloat {
        return activePen().smoothness(forCoalescedTouch: coalescedTouch, from: touch)
     }
 
-    
-    
-    
     //pragma mark - JotViewStateProxyDelegate
     
     func documentDir() -> String {
@@ -322,51 +210,6 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
         
     }
     
-    
-    
-    /*
-    @property(nonatomic, readonly) NSString* jotViewStateInkPath;
-    
-    @property(nonatomic, readonly) NSString* jotViewStatePlistPath;
-    
-    - (void)didLoadState:(JotViewStateProxy*)state;
-    
-    - (void)didUnloadState:(JotViewStateProxy*)state;
- 
-    */
-    
-    
-    
-    
-    
-    
-    
-    
-    
-/*
-    
-    
-    - (NSString*)jotViewStateInkPath {
-    return [[self documentsDir] stringByAppendingPathComponent:@"ink.png"];
-    }
-    
-    - (NSString*)jotViewStateThumbPath {
-    return [[self documentsDir] stringByAppendingPathComponent:@"thumb.png"];
-    }
-    
-    - (NSString*)jotViewStatePlistPath {
-    return [[self documentsDir] stringByAppendingPathComponent:@"state.plist"];
-    }
-    
-    - (void)didLoadState:(JotViewStateProxy*)state {
-    }
-    
-    - (void)didUnloadState:(JotViewStateProxy*)state {
-    }
-
-    */
-    
-    
     //MARK: - InputObject Delegate
     
     //this gets called when moving a blockGroup and when a block from the inputObject is being dragged around
@@ -375,7 +218,6 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
             if(group != _movedView){
                 if(group.isNear(incomingView: _movedView)){
                     if(group.isDisplayingSpots == false){
-                        
                         group.findAndShowAvailableSpots(_movedView: _movedView)
                         //this will send the message to "group" that it needs to show its available spots for movedView
                     }
@@ -474,11 +316,9 @@ class DeskViewController: UIViewController, UIScrollViewDelegate, UIGestureRecog
                                     group.addSubview(sub)
                                 }
                             }
-                            
                             //get rid of old expression, may need to make sure that there are no more references
                             expressions.removeObject(object: expression)
                             expression.isHidden = true
-                            
                         }
                     }
                 }
