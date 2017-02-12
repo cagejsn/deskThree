19ebc2610e1c7d9db3fd2d5da7cf244a972c056c
Arjun Nagineni <arjun.nagineni@utexas.edu>
2017-02-11 22:33:56 -0600

    trying to override hit test

--- deskThree/deskThree/DeskView.swift
+++ deskThree/deskThree/DeskView.swift
@@ -35,4 +35,21 @@ class DeskView: UIView {
         
     }
     
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if (!self.isUserInteractionEnabled || self.isHidden || self.alpha <= 0.01) {
+            return nil
+        }
+        if(self.point(inside: point, with: event)){
+            for subView: UIView in self.subviews {
+                var convertedPoint = self.convert(point, to: subView)
+                var hitTestView = subView.hitTest(convertedPoint, with: event)
+                if((hitTestView) != nil){
+                    return hitTestView
+                }
+            }
+            return self
+        }
+        return nil
+    }
+    
 }
