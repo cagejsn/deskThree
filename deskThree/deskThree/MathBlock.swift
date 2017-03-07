d79034b5adc96e01d837c79eae4478c9fe2a6047
Arjun Nagineni <arjun.nagineni@utexas.edu>
2017-03-07 13:24:45 -0600

    Added longPressGR. Messing with block sizes

--- deskThree/deskThree/MathBlock.swift
+++ deskThree/deskThree/MathBlock.swift
@@ -9,19 +9,29 @@
 import Foundation
 
 class MathBlock: Expression{
-    var imageHolder: UIImageView!
-    var mathSymbols: [MAWSymbol] = []
+    private var imageHolder: UIImageView!
+    private var mathSymbols: [MAWSymbol] = []
+    private var longPressGR: UILongPressGestureRecognizer!
+    
+    func handleLongPress(){
+        
+    }
     
     init(image: UIImage, symbols: NSArray, text: String){
-        super.init(frame: CGRect(x:0, y:0, width: 200, height: 100))
+        var frame = image.size
+        super.init(frame: CGRect(frame))
+        print(image.size)
         // Image setup
-        imageHolder = UIImageView(frame: CGRect(x:0, y:0, width: 200, height: 40));
+        imageHolder = UIImageView(frame: CGRect(x:0, y:0, width: 70, height: 40));
         imageHolder.contentMode = .scaleAspectFit
-        imageHolder.image = image;
+        imageHolder.image = image
         self.addSubview(imageHolder)
         
         mathSymbols = symbols as! [MAWSymbol]
         expressionString = text
+        longPressGR = UILongPressGestureRecognizer(target: self, action: #selector(MathBlock.handleLongPress))
+        longPressGR.minimumPressDuration = 0.5
+        self.addGestureRecognizer(longPressGR)
     }
     
     required init?(coder unarchiver: NSCoder) {
