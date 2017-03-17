5bf61309bcd157e9fa39da6eb0f030368ec994d8
Cage <cagejsn@gmail.com>
2017-03-16 19:23:10 -0500

    a couple more changes to the file explorer

--- deskThree/deskThree/FileExplorerViewController.swift
+++ deskThree/deskThree/FileExplorerViewController.swift
@@ -9,7 +9,23 @@
 import Foundation
 
 
-class FileExplorerViewController: UIViewController {
+class FileExplorerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet var tableView: UITableView!
 
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 60))
+        cell.backgroundColor = UIColor.purple
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 }
