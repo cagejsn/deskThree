e0883f9fa79c744d85624204e9819ca3394546ca
Benjamin28 <Benjamin.Baker@utexas.edu>
2017-03-18 17:18:07 -0500

    Added path locator class which directs user to folders to save and load from

---
+++ deskThree/deskThree/PathLocator.swift
@@ -0,0 +1,71 @@
+//
+//  PathLocator.swift
+//  deskThree
+//
+//  Created by test on 3/18/17.
+//  Copyright © 2017 desk. All rights reserved.
+//
+
+import Foundation
+
+class PathLocator {
+    
+    ///Returns string to project folder location
+    static func getProjectFolder() -> String{
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let fileManager = FileManager.default
+        let deskFolder = documentsPath+"/DeskThree"
+        if (!fileManager.fileExists(atPath: deskFolder)){
+            print("D3 folder missing. Initializing...")
+            
+            do {
+                try fileManager.createDirectory(atPath: deskFolder, withIntermediateDirectories: false, attributes: nil)
+            } catch let error as NSError {
+                print(error.localizedDescription);
+            }
+        }
+        let projectsFolder = deskFolder+"/Projects"
+        if (!fileManager.fileExists(atPath: projectsFolder)){
+            print("Projects folder missing. Initializing...")
+            
+            do {
+                try fileManager.createDirectory(atPath: projectsFolder, withIntermediateDirectories: false, attributes: nil)
+            } catch let error as NSError {
+                print(error.localizedDescription);
+            }
+        }
+        return projectsFolder
+    }
+    
+    ///Returns string to metadata folder location
+    static func getMetaFolder() -> String{
+        
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let fileManager = FileManager.default
+        let deskFolder = documentsPath+"/DeskThree"
+        if (!fileManager.fileExists(atPath: deskFolder)){
+            print("D3 folder missing. Initializing...")
+            
+            do {
+                try fileManager.createDirectory(atPath: deskFolder, withIntermediateDirectories: false, attributes: nil)
+            } catch let error as NSError {
+                print(error.localizedDescription);
+            }
+        }
+        let metaFolder = deskFolder+"/Meta"
+        if (!fileManager.fileExists(atPath: metaFolder)){
+            print("Meta data folder missing. Initializing...")
+            
+            do {
+                try fileManager.createDirectory(atPath: metaFolder, withIntermediateDirectories: false, attributes: nil)
+            } catch let error as NSError {
+                print(error.localizedDescription);
+            }
+        }
+        return metaFolder
+
+        
+    }
+    
+    
+}
