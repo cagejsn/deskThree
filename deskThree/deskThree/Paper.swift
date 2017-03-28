2ac9a3f15629676013f114e67f98d195662fbaa8
Arjun Nagineni <arjun.nagineni@utexas.edu>
2017-03-28 11:34:26 -0500

    Cleaned up unnecessary code in Paper

--- deskThree/deskThree/Paper.swift
+++ deskThree/deskThree/Paper.swift
@@ -59,8 +59,6 @@ class Paper: UIImageView, ImageBlockDelegate, ExpressionDelegate {
     }
     
     
-    
-    
     func addMathBlockToPage(block: MathBlock){
         block.delegate = self
         expressions.append(block)
@@ -99,90 +97,6 @@ class Paper: UIImageView, ImageBlockDelegate, ExpressionDelegate {
 
     }
     
-    // This is never called
-    private func setupGestureRecognizers() {
-        // 1. Set up a pan gesture recognizer to track where user moves finger
-        let panRecognizer = UIPanGestureRecognizer(target: self, action: Selector(("handlePan")))
-        self.addGestureRecognizer(panRecognizer)
-    }
-    
-    @objc func handlePan(sender: UIPanGestureRecognizer) {
-        let point = sender.location(in: self)
-        switch sender.state {
-        case .began:
-            self.startAtPoint(point: point)
-        case .changed:
-            self.continueAtPoint(point: point)
-        case .ended:
-            self.endAtPoint(point: point)
-        case .failed:
-            self.endAtPoint(point: point)
-        default:
-            assert(false, "State not handled")
-        }
-    }
-    
-    
-    func drawLine(a: CGPoint, b: CGPoint, buffer: UIImage?) -> UIImage {
-        let size = self.bounds.size;
-        
-        UIGraphicsBeginImageContextWithOptions(size, true, 0)
-        let context = UIGraphicsGetCurrentContext()
-        self.sendSubview(toBack: self)
-        context!.setFillColor(self.backgroundColor?.cgColor ?? UIColor.white.cgColor)
-        context!.fill(self.bounds)
-        
-        // Draw previous buffer first
-        if let buffer = buffer {
-            buffer.draw(in: self.bounds)
-        }
-        
-        // Draw the line
-        self.drawColor.setStroke()
-        self.path.lineWidth = self.drawWidth
-        self.path.lineCapStyle = CGLineCap.round
-        self.path.stroke()
-        context!.setLineWidth(self.drawWidth)
-        context!.setLineCap(CGLineCap.round)
-        
-        context!.move(to: a)
-        context!.addLine(to: b)
-        context!.strokePath()
-        
-        // Grab the updated buffer
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
-    }
-    
-    func startAtPoint(point: CGPoint) {
-        self.lastPoint = point
-    }
-    
-    func continueAtPoint(point: CGPoint) {
-        autoreleasepool {
-            // Draw the current stroke in an accumulated bitmap
-            self.buffer = self.drawLine(a: self.lastPoint, b: point, buffer: self.buffer)
-            
-            // Replace the layer contents with the updated image
-            self.image = self.buffer
-            
-            // Update last point for next stroke
-            self.lastPoint = point
-        }
-    }
-    
-    func endAtPoint(point: CGPoint) {
-        self.lastPoint = CGPoint.zero
-    }
-    
-    var drawColor: UIColor = UIColor.black
-    var drawWidth: CGFloat = 10.0
-    
-    private var path: UIBezierPath = UIBezierPath()
-    private var lastPoint: CGPoint = CGPoint.zero
-    private var buffer: UIImage = UIImage(named: "engineeringPaper")!
-    
     
     func setupDelegateChain(){
         for image in images {
@@ -203,9 +117,6 @@ class Paper: UIImageView, ImageBlockDelegate, ExpressionDelegate {
     //MARK: Initializers
     init() {
         super.init(frame: CGRect(x: 10, y: 10, width: 400, height: 400))
-        //   longPressGR = UILongPressGestureRecognizer(target: self, action: #selector(Paper.handleLongPress(sender:)))
-        //     longPressGR.minimumPressDuration = 0.8
-        //     self.addGestureRecognizer(longPressGR)
         expressions = [BlockExpression]()
         self.image = UIImage(named: "engineeringPaper2")
         self.isOpaque = false
