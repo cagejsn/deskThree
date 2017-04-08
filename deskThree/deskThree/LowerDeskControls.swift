4d22265eda73a19cf5ccb28801d3933b39b7ead1
Zachary A. Keener <zkeener@leaf.ag>
2017-04-08 15:12:04 -0500

    added mixpanel events

--- deskThree/deskThree/LowerDeskControls.swift
+++ deskThree/deskThree/LowerDeskControls.swift
@@ -7,19 +7,24 @@
 //
 
 import Foundation
-
-
+import Mixpanel
 
 class LowerDeskControls: UIView {
     
     var delegate: DeskControlModuleDelegate!
+    
+    // Mixpanel initialization
+    var mixpanel = Mixpanel.initialize(token: "4282546d172f753049abf29de8f64523")
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.clipsToBounds = true
-        }
+    }
     
     @IBAction func lastPageTapped(_ sender: Any) {
+        // Mixpanel event
+        mixpanel.track(event: "Button: Page Left")
+
         delegate.lastPageTapped(self)
     }
 
@@ -32,6 +37,9 @@ class LowerDeskControls: UIView {
     }
     
     @IBAction func nextPageTapped(_ sender: Any) {
+        // Mixpanel event
+        mixpanel.track(event: "Button: Page Right")
+        
         delegate.nextPageTapped(self)
     }
 }
